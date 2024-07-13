d52ddb861203c1e7e9a6b9742c289de026a9cfec
rajeevrj256 <rjrajeev5918@gmail.com>
2024-07-13 22:22:58 +0530

    auth user name and  dp on sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,15 +3,15 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import OtherUsers from './OtherUsers';
 import axios from "axios";
 import toast from "react-hot-toast";
-import {useNavigate} from "react-router-dom";
-import {useSelector, useDispatch} from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { setAuthUser, setOtherUsers, setSelectedUser } from '../redux/userSlice';
 import { setMessages } from '../redux/messageSlice';
 import { BASE_URL } from '..';
- 
+
 const Sidebar = () => {
     const [search, setSearch] = useState("");
-    const {OtherUsers} = useSelector(store=>store.user);
+    const { OtherUsers, AuthUser } = useSelector(store => store.user);
     const dispatch = useDispatch();
 
     const navigate = useNavigate();
@@ -29,13 +29,13 @@ const Sidebar = () => {
             console.log(error);
         }
     }
-    
+
     const searchSubmitHandler = (e) => {
         e.preventDefault();
-        const conversationUser = OtherUsers?.find((user)=> user.fullName.toLowerCase().includes(search.toLowerCase()));
-        if(conversationUser){
+        const conversationUser = OtherUsers?.find((user) => user.fullName.toLowerCase().includes(search.toLowerCase()));
+        if (conversationUser) {
             dispatch(setOtherUsers([conversationUser]));
-        }else{
+        } else {
             toast.error("User not found!");
         }
     }
@@ -44,19 +44,30 @@ const Sidebar = () => {
             <form onSubmit={searchSubmitHandler} action="" className='flex items-center gap-2'>
                 <input
                     value={search}
-                    onChange={(e)=>setSearch(e.target.value)}
+                    onChange={(e) => setSearch(e.target.value)}
                     className='input input-bordered rounded-md' type="text"
                     placeholder='Search...'
                 />
                 <button type='submit' className='btn bg-zinc-700 text-white'>
-                    <BiSearchAlt2 className='w-6 h-6 outline-none'/>
+                    <BiSearchAlt2 className='w-6 h-6 outline-none' />
                 </button>
             </form>
-            <div className="divider px-3"></div> 
-            <OtherUsers/> 
+            <div className="divider px-3"></div>
+            <OtherUsers />
             <div className='mt-2'>
                 <button onClick={logoutHandler} className='btn btn-sm'>Logout</button>
             </div>
+            <div>
+                <div>
+
+                    <img src={AuthUser?.profilePhoto} alt="user-profile" />
+                </div>
+                <div>
+                    <p>{AuthUser?.fullName}</p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
